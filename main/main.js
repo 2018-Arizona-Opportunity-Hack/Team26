af8bf4f88abe6ba243e43c0981845ba4f66b7398
Saul Lopez <slopez15@asu.edu>
2018-10-21 00:03:07 -0700

    top package script to run frontend and db script

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
 /*
 To bring together and run application.
 */
-console.log("running?")
+console.log("running?");
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+console.log(process.env.DATABASE_URL);
+console.log("still running?");
+
+/*
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+*/
