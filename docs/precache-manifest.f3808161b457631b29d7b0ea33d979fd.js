bec0963a736270928744b401648ab7db11fd54e2
David Baxter <baxter30@gmail.com>
2018-10-21 15:36:28 -0700

    added docs folder

---
+++ docs/precache-manifest.f3808161b457631b29d7b0ea33d979fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2958208318f6ecdafc52d6da2c0ba347",
+    "url": "/static/media/swift-logo.29582083.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0749a0e4e39951779c49",
+    "url": "/static/js/main.0749a0e4.chunk.js"
+  },
+  {
+    "revision": "1a8c70d200a7cd65096b",
+    "url": "/static/js/1.1a8c70d2.chunk.js"
+  },
+  {
+    "revision": "0749a0e4e39951779c49",
+    "url": "/static/css/main.f247f19d.chunk.css"
+  },
+  {
+    "revision": "1a8c70d200a7cd65096b",
+    "url": "/static/css/1.a0cbbcc1.chunk.css"
+  },
+  {
+    "revision": "2cf33bfd6da35a90d78b2a3bee0de898",
+    "url": "/index.html"
+  }
+];
