95c946e45e569fc2e1f93e08f1fb4264924f424b
David Baxter <baxter30@gmail.com>
2018-10-21 15:30:41 -0700

    uploading build

---
+++ precache-manifest.cdb7f2e337dab2a2d0252bfbe58615bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2958208318f6ecdafc52d6da2c0ba347",
+    "url": "/static/media/swift-logo.29582083.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "972a8dd65c5834fd2f90",
+    "url": "/static/js/main.972a8dd6.chunk.js"
+  },
+  {
+    "revision": "1a8c70d200a7cd65096b",
+    "url": "/static/js/1.1a8c70d2.chunk.js"
+  },
+  {
+    "revision": "972a8dd65c5834fd2f90",
+    "url": "/static/css/main.5636817f.chunk.css"
+  },
+  {
+    "revision": "1a8c70d200a7cd65096b",
+    "url": "/static/css/1.a0cbbcc1.chunk.css"
+  },
+  {
+    "revision": "2d89b337eaed1e7351ac1ee7d93b9156",
+    "url": "/index.html"
+  }
+];
